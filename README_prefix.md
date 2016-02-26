6162e659f403f32cb1d6b2df607c1a4ad2a22d84
tomsik68 <tomsik68@gmail.com>
2016-02-26 14:21:08 +0000

    Added README_prefix and script to update README

---
+++ README_prefix.md
@@ -0,0 +1,5 @@
+# Templ8tor templates
+
+Templ8tor is a free, open-source tool that processes templates. This is my public collection of templates for templ8tor.
+Feel free to use them for any purpose. You can also submit pull requests with your own templates :)
+Please see CONTRIBUTING.md for contributing guidelines.
---
+++ updatereadme.sh
@@ -0,0 +1,36 @@
+#!/bin/bash
+cp README_prefix.md README.md
+FILES=`ls *.templ`
+for FILE in $FILES; do
+	echo -e "## `echo "$FILE" | sed s':.templ::g'`\n" >> README.md
+	VARIABLES=''
+	AUTHOR=''
+	DESC=''
+	while read -r LINE; do
+		case $LINE in
+			"-->")
+				break
+				;;
+			Author:*)
+				AUTHOR=$LINE
+				;;
+			Variables:*)
+				# GFM table!
+				VARIABLES="|Name|Description|\n|:-----:|:----------------------|\n"
+				;;
+			*)
+				if [[ $VARIABLES = '' ]]; then
+					DESC=$LINE
+				else
+					VARNAME=`echo "$LINE" | sed s':-.*::g'`
+					VARDESC=`echo "$LINE" | sed s':.*-::g'`
+					VARIABLES="$VARIABLES | \`$VARNAME\` | $VARDESC | \n"
+				fi
+				;;
+			esac
+		done < $FILE
+		echo -e "$DESC\n" >> README.md
+		echo -e "$AUTHOR\n" >> README.md
+		echo -e "Variables: \n$VARIABLES" >> README.md
+	done
+	IFS=$BACKUPIFS
